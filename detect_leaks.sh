911ac68d76cb98c833064366c83e87f4eec8fb68
JeremyRand <jeremyrand@airmail.cc>
2019-11-03 17:17:08 +0000

    Add copyright header to bash script

--- detect_leaks.sh
+++ detect_leaks.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Copyright 2017-2019 Jeremy Rand.
+#
+# This file is part of Heteronculous.
+#
+# Heteronculous is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Heteronculous is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Heteronculous.  If not, see
+# <https://www.gnu.org/licenses/>.
+
 set -euf -o pipefail
 
 whitelist="^[0-9]\+ \+socket(AF_UNIX,
